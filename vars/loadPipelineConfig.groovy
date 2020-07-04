d1282d9e4fd53b75001df487c8c2983b2fc735c9
Hoa Mai <hoa.dinhmai@gmail.com>
2020-07-04 19:36:57 +1000

    Remove script

--- vars/loadPipelineConfig.groovy
+++ vars/loadPipelineConfig.groovy
@@ -1,7 +1,7 @@
 def call(Map parameters = [:]) {
     try {
         configFile = 'resources/config/nonprod.yaml'
-        Map config = script.readYaml file: configFile
+        Map config = readYaml file: configFile
         return config
     } catch (Exception e) {
         echo "WARNING: Could not load data from config file at ${configFile}. Exception: ${e.getMessage()}"
